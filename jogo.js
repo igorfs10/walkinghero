1cae724575ad97079d589c7273e9fc8a41c6a5c2
igorfs10 <igorfs10@gmail.com>
2018-06-25 19:13:28 -0300

    Lidando com alguns erros e melhoria na interface

--- jogo.js
+++ jogo.js
@@ -1,10 +1,8 @@
 var document = document,
 	time = 0,
+	jogando = false,
 	tempo = document.getElementById("txtTempo");
-
-tempo.innerHTML = "Tempo: " + time + "s";
-setTimeout(contarTempo,1000);
-
+	
 function contarTempo(){
 	time = time + 1;
 	tempo.innerHTML = "Tempo: " + time + "s";
@@ -14,13 +12,27 @@ function contarTempo(){
 function novoJogo(){
 	time = 0;
 	tempo.innerHTML = "Tempo: " + time + "s";
+	setTimeout(contarTempo,1000);
+	jogando = true;
 }
 
 function salvar(){
-	localStorage.setItem("tempinho", time);
+	if(jogando === true){
+		localStorage.setItem("tempinho", time);
+	} else {
+		alert("Precisa estar jogando para salvar o jogo.");
+	}
 }
 
 function carregar(){
-	time = parseInt(localStorage.getItem("tempinho"));
-	tempo.innerHTML = "Tempo: " + time + "s";
+	if (isNaN(parseInt(localStorage.getItem("tempinho")))){
+		alert("Não tem arquivo salvo");
+	} else {
+		time = parseInt(localStorage.getItem("tempinho"));
+		tempo.innerHTML = "Tempo: " + time + "s";
+		if (jogando === false){
+			setTimeout(contarTempo,1000);
+			jogando = true;
+		}
+	}
 }
