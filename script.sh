b47d66b40766852c1ea9b96ef61d3e3169ddab4d
igorfs10 <igorfs10@gmail.com>
2019-09-07 15:44:33 -0300

    Melhorias no script

--- script.sh
+++ script.sh
@@ -7,9 +7,8 @@ dart2js -O4 -o javascript/teste.js main.dart
 
 cp javascript/teste.js ${REPO}/javascript
 cd ${REPO}
-git config user.email "travis@travis-ci.org"
-git config user.name "travisci"
-git remote set-url origin "https://${GITHUB_TOKEN}@${GH_REPO}" ${BRANCH}
+git config user.email "${EMAIL}"
+git config user.name "${USER}"
 git add *
-git commit -m "Travis build $TRAVIS_COMMIT pushed to gh-pages"
+git commit -m "Travis build $TRAVIS_COMMIT pushed to ${BRANCH}"
 git push "https://${GITHUB_TOKEN}@${GH_REPO}" ${BRANCH}
