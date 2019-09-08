8237ebd0119dda08c6f11eee6781670186fe117f
igorfs10 <igorfs10@gmail.com>
2019-09-08 03:13:24 -0300

    arrumar o travis

--- main.dart
+++ main.dart
@@ -1,3 +1,11 @@
+import "main.dart";
+import "constantes/inimigos.dart";
+import "constantes/itens.dart";
+import "dados/dadosItens.dart";
+import "models/Inimigo.dart";
+import "models/InimigoBatalha.dart";
+import "models/Item.dart";
+
 main(){
   print("Teste.");
 }
